bf306c2e2cd4f64e1f7e77ee730e7f4926f00d27
Kaziamov <kaziamov@outlook.com>
2024-04-05 16:34:09 +0400

    Add tg bot disable

--- backend/settings.py
+++ backend/settings.py
@@ -16,3 +16,4 @@ RANDON_WEBHOOK_PATH = "".join(random.choices(list(ascii_letters + digits), k=32)
 WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", RANDON_WEBHOOK_PATH)
 
 ENABLE_TGBOT = os.getenv("ENABLE_TGBOT", True)
+RETRY_TIMEOUT = int(os.getenv("RETRY_TIMEOUT", 60))
